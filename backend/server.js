0eab37e236858d5247d0bd23a96dbbd50804110a
M-Hamza-Yousaf <hamzayousafansari@gmail.com>
2026-05-26 11:44:11 +0500

    add vercel config for backend

--- backend/server.js
+++ backend/server.js
@@ -25,3 +25,6 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+
+// Export for Vercel
+module.exports = app;h
